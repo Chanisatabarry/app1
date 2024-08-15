6b1f97bb58ad097dcf233b12abca0f9102e17b2d
Chanisatabarry <156068781+Chanisatabarry@users.noreply.github.com>
2024-08-15 20:00:24 +0700

    Calculator2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import Button from './class-components'
+import Calculator2 from './calculator';
 
-function App(){
- return <Button/>
-}
-export default App
+export default function App(){
+  return <Calculator2/>
+ }
