64022fc38dc4c0bf22f3f4833d651005a40791b1
Chanisatabarry <156068781+Chanisatabarry@users.noreply.github.com>
2024-08-15 19:06:13 +0700

    Button

--- src/class-components.js
+++ src/class-components.js
@@ -1,17 +1,13 @@
 import React,{Component} from 'react'
 
-export default class Calendar extends Component{
-getDate(){
-    const dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'june', 'july', 'August', 'September', 'October', 'November','December']
-    const date = new Date()
-    const weekDay = dayNames[date.getDay()]
-    const day = date.getDate()
-    const month = monthNames[date.getMonth()]
-    const year = date.getFullYear()//พ.ศ.
-    return` ${weekDay} ${day} ${month} ${year}`// `ต้องใช้ตัวนี้เท่านั้น Alt+96`
-}    
+export default class Button extends Component{
+    showAlert(msg){
+        alert(msg)
+    }
+    onClickButtonOK = () =>{
+        this.showAlert('Hello')
+    }
 render(){
-    return <div>{this.getDate()}</div>
+    return <button onClick={this.onClickButtonOK}>OK</button>
 }
 }
